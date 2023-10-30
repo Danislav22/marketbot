f2ba85e14bb17d84e279221347a039c09f2b5420
Viacheslav <quidicorporation@gmail.com>
2023-10-30 21:25:01 +0700

    Update steam_cookies.py

--- secret_data/steam_cookies.py
+++ secret_data/steam_cookies.py
@@ -1,12 +1,2 @@
 cookies = {
-    "sessionid": "b93ea3dfe9fc0d1fc7fb3bac",
-    "strInventoryLastContext": "730_2",
-    "tsTradeOffersLastRead": "1698578314",
-    "steamCurrencyId": "5",
-    "steamLoginSecure": "76561198220991936%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MEQ1Q18yMzYxRTZCNl85QTc0NSIsICJzdWIiOiAiNzY1NjExOTgyMjA5OTE5MzYiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY5ODc0MjI5NSwgIm5iZiI6IDE2OTAwMTQ4MjksICJpYXQiOiAxNjk4NjU0ODI5LCAianRpIjogIjBENEFfMjM2NUUxMUVfNUZFQkYiLCAib2F0IjogMTY5ODIyNjE2NSwgInJ0X2V4cCI6IDE3MTYxNDUxNDksICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICI3OC4xMzkuMTk5LjUwIiwgImlwX2NvbmZpcm1lciI6ICI3OC4xMzkuMTk5LjUwIiB9.V6ZMQAtNfAxlLiShZ2f1jnoZK68oQ0ED2eAb87_ThWNd6Bfbuyv4HkNX1Uhpo_J8AYbJw6TGU8I1hbUhqjSZBg",
-    "timezoneOffset": "25200,0",
-    "webTradeEligibility": "%7B%22allowed%22%3A1%2C%22allowed_at_time%22%3A0%2C%22steamguard_required_days%22%3A15%2C%22new_device_cooldown_days%22%3A0%2C%22time_checked%22%3A1698674291%7D",
-    "browserid": "2960613309323571698",
-    "Steam_Language": "english",
-    "ActListPageSize": "10",
 }
